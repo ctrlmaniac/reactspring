579a6a1b5f7792ea60ca8db2afe22b8d35d4a989
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-16 17:27:01 +0100

    add java project

---
+++ src/main/java/me/ctrlmaniac/reactspring/ReactspringApplication.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.reactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ctrlmaniac/reactspring/ReactspringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.reactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
